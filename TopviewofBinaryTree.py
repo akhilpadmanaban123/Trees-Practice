523ec9b1eacbdbab054ebc4c5fa6a9f02fb894b6
Akhil Padmanaban_ <70253347+akhilpadmanaban123@users.noreply.github.com>
2023-04-06 07:09:44 +0530

    Update TopviewofBinaryTree.py

--- TopviewofBinaryTree.py
+++ TopviewofBinaryTree.py
@@ -1,2 +1,13 @@
 def TopView(root):
-  
+  if root is None:
+    return 
+  queue=[(root,0)]
+  d={}
+  while queue:
+    i,j=queue.pop(0)
+    if j not in d:
+      d[j]=i.info
+    queue.append((root.left,j-1))
+    queue.append((root.right,j+1))
+  for i in sorted(d):
+    print(d[i],end=' ')
