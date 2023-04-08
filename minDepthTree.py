5c7a556f37c5e07d3aa5735fb6a58cf35ff067c9
Akhil Padmanaban_ <70253347+akhilpadmanaban123@users.noreply.github.com>
2023-04-08 07:56:23 +0530

    Update minDepthTree.py

--- minDepthTree.py
+++ minDepthTree.py
@@ -1,4 +1,27 @@
-queue=[]
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        
+        if root.left is None:
+            return 1+self.minDepth(root.right)
+        
+        if root.right is None:
+            return 1+self.minDepth(root.left)
+
+        return min(self.minDepth(root.left),self.minDepth(root.right))+1
+        
+
+        '''
+        queue=[]
         if root is None:
             return 0
         if root is not None:
@@ -16,3 +39,5 @@ queue=[]
                     queue.append(cur.right)
             depth+=1
         return depth
+
+        '''
