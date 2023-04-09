a6851296e9a437c0af4a669b4b8d4698a775e78e
Akhil Padmanaban_ <70253347+akhilpadmanaban123@users.noreply.github.com>
2023-04-09 07:29:56 +0530

    Update pathSum.py

--- pathSum.py
+++ pathSum.py
@@ -13,3 +13,30 @@ class Solution:
             return True
         
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right,targetSum)
+    
+    
+    
+#---------------------------------
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+        queue=[(root,root.val)]
+        while queue:
+            node, val=queue.pop(0)
+            if node.left is not None:
+                queue.append((node.left, val+node.left.val))
+            if node.right is not None:
+                queue.append((node.right, val+node.right.val))
+            if node.left is None and node.right is None:
+                if val==targetSum:
+                    return True
+                else:
+                    continue
+        return False
